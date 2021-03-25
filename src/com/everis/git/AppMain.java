5230144a8833ed851bec63d452ed0e316bd85152
scarnero <scarnero@everis.com>
2021-03-25 10:43:21 +0100

    Versión 1 - GIT

---
+++ src/com/everis/git/AppMain.java
@@ -0,0 +1,21 @@
+package com.everis.git;
+
+/**
+ * Formacion Dual - GIT Desafio 1 
+ * 
+ * @author everis
+ *
+ */
+public class AppMain {
+
+	/**
+	 * Metodo principal
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hola Mundo");
+	}
+
+}
